56c668b2dc88955a73f5558ecb92aace4f3c17eb
Marcus Ambler <amblermarcus@gmail.com>
2025-05-24 21:28:44 +1000

    initial

---
+++ website/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  return (
+    <div className="min-w-screen min-h-screen bg-gray-900 flex flex-col items-center justify-center p-4">
+      <h1 className="text-4xl font-bold text-gray-100 mb-4">
+        Hello
+      </h1>
+      {/* <p className="text-lg text-gray-100">
+        D
+      </p> */}
+      <button className="mt-2 px-4 py-2 bg-indigo-500 font-medium text-white rounded-lg shadow-md hover:bg-indigo-700 transition duration-300">
+        Continue
+      </button>
+    </div>
+    
+  )
+}
+
+export default App
