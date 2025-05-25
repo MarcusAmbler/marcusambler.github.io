79b17f73605738874fadcca3eddbc88c3ec19ff9
Marcus Ambler <amblermarcus@gmail.com>
2025-05-25 22:02:25 +1000

    first cards

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,21 +1,30 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import aceImage from './assets/ace1.png';
+import threeImage from './assets/threeSp.png';
 
 function App() {
+  const aceCardRef = useRef(null);
+  const threeCardRef = useRef(null);
+
   return (
-    <div className="min-w-screen min-h-screen bg-gray-900 flex flex-col items-center justify-center p-4">
+    <div className="min-w-screen min-h-screen bg-neutral-900 flex flex-col items-center justify-center p-4">
       <h1 className="text-4xl font-bold text-gray-100 mb-4">
         Hello
       </h1>
-      {/* <p className="text-lg text-gray-100">
-        D
-      </p> */}
+      <div className="flex flex-row">
+        <div ref={aceCardRef} className="pixel-card three-card-start-position">
+          <img src={aceImage} alt="Ace Card" className="h-64" style={{ imageRendering: 'pixelated' }}/>
+        </div>
+        <div ref={threeCardRef} className="pixel-card three-card-start-position">
+          <img src={threeImage} alt="three Card" className="h-64" style={{ imageRendering: 'pixelated' }}/>
+        </div>
+      </div>
       <button className="mt-2 px-4 py-2 bg-indigo-500 font-medium text-white rounded-lg shadow-md hover:bg-indigo-700 transition duration-300">
         Continue
       </button>
     </div>
-    
   )
 }
 
